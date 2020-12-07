9d3e74c96e7d306b15d10b2169dc516a2cf1edd7
Your Name <you@example.com>
2020-12-07 14:48:13 -0600

    Created a basic Express JS Web Server.

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Hello Rishav");
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  //res.send("Hello, GFG");
+  res.json(`Hello, GFGS`);
+});
+
+app.listen(port, () => {
+  console.log(`Server started in port ${port}.`);
+});
