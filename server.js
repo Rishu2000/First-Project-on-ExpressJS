e1a862b2187442880836564122b52abf2e3af3fd
Your Name <you@example.com>
2020-12-07 19:49:39 -0600

    Implemented a moduler routing technique.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const root = require("./routes/root");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
-  //res.send("Hello, GFG");
-  res.json(`Hello, GFGS`);
-});
+app.use("/", root);
+
+app.use("/", root);
 
 app.listen(port, () => {
   console.log(`Server started in port ${port}.`);
